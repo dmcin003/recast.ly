46071aa7942602cfeeefd249e2f1e1851d21a435
dmcin003 <dmcin003@fiu.edu>
2021-10-05 11:40:32 -0400

    "Complete creating a dynamic Video List component"

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,28 +1,14 @@
 import VideoListEntry from './videoListEntry.js';
 
-class VideoList extends React.Component {
 
+var VideoList = (props) => (
+  <div className="video-list">
+    {props.videos.map((video) => {
+      return <VideoListEntry key={video.id.videoId} video={video}/>;
+    })}
+  </div>
+);
 
-  constructor(props) {
-    super(props);
-  }
-
-  console.log(props.videos);
-
-  render() {
-
-    return (
-      <div className="video-list">
-        {props.videos.map((video) =>{
-          return <div><VideoListEntry video={video}/></div>;
-
-        })}
-      </div>
-    );
-
-  }
-
-}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
